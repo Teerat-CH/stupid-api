1e6b8c05b704ab34004581e1acceec68077f4063
Teerat Chanromyen <teerat.nahm@gmail.com>
2026-01-16 08:33:34 +0700

    feat/auto deploy image through github action

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import random
 
 app = FastAPI()
 
@@ -9,3 +10,10 @@ def read_root():
 @app.post("/echo")
 def echo_string(text: str):
     return {"received": text, "response": f"You said: {text}"}
+
+@app.post("/sarcastic")
+def sarcastic_response(text: str):
+    return ''.join(
+        char.upper() if char.isalpha() and random.random() < 0.5 else char.lower()
+        for char in text
+    )
